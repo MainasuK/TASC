abb8c53e9440c27e1e469fd107a63d90fcb61da5
MainasuK <cirno.mainasuk@gmail.com>
2015-01-06 11:39:40 +0800

    init wit "Hello world"

---
+++ ch2/2.1_Hello_world/2.1_Hello_world/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  2.1_Hello_world
+//
+//  Created by Cirno MainasuK on 2015-1-6.
+//
+//  File: hello.c
+//  -------------
+//  This program prints the message "Hello, world."
+//  on the screen. The program is taken from the
+//  classic C reference text "The C Programming
+//  Language" by Brian Kernighan and Dennis Ritchie.
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, world.\n");
+    
+    return 0;
+}
