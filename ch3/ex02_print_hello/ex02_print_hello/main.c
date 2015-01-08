a08b66435d1f61ff9e810fdff613dc73b0bf84e3
MainasuK <cirno.mainasuk@gmail.com>
2015-01-08 21:44:22 +0800

    ex.02_print hello world ten times

---
+++ ch3/ex02_print_hello/ex02_print_hello/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  ex02_print_hello
+//
+//  Created by Cirno MainasuK on 2015-1-8.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    for (int i = 0; i < 10; i++) {
+        printf("Hello, world.\n");
+    }
+
+    return 0;
+}
