f8c6ecb4bbbd2e5347573f9e2ecd5453a90cf47f
MainasuK <cirno.mainasuk@gmail.com>
2015-01-08 19:21:27 +0800

    ex.8_math magic problem

---
+++ ch2/ex8_math_magic/ex8_math_magic/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  ex8_math_magic
+//
+//  Created by Cirno MainasuK on 2015-1-8.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int anwser =  4 + 9 - 2 * 16 + 1 / 3 * 6 - 67 + 8 * 2 - 3 + 26 - 1 / 34 + 3 / 7 + 2 - 5;
+    printf("%d", anwser);
+    
+    return 0;
+}
