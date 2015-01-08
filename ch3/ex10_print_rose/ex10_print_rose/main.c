fcf90791c40f4815e81cc4383d106dc563811674
MainasuK <cirno.mainasuk@gmail.com>
2015-01-08 22:22:44 +0800

    ex.10_print a rose with TIMES times

---
+++ ch3/ex10_print_rose/ex10_print_rose/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  ex10_print_rose
+//
+//  Created by Cirno MainasuK on 2015-1-8.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+#include <stdio.h>
+#define TIMES 2
+int main(int argc, const char * argv[]) {
+    for (int i = 0; i < TIMES; i++) {
+        printf("arose is ");
+    }
+    printf("a rose.\n");
+    return 0;
+}
