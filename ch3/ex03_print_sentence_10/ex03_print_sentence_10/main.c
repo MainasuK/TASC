f4718c19f47a0382e2bd1245934fd226496ad527
MainasuK <cirno.mainasuk@gmail.com>
2015-01-08 21:46:51 +0800

    ex.03_print sentence * 10

---
+++ ch3/ex03_print_sentence_10/ex03_print_sentence_10/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  ex03_print_sentence_10
+//
+//  Created by Cirno MainasuK on 2015-1-8.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    for (int i = 0; i < 10; i++) {
+        printf("Tomorrow and tomorrow and tomorrow.\n");
+    }
+    return 0;
+}
