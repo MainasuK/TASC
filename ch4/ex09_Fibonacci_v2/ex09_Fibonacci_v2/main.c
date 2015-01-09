387769d03e33c72acb242c7d045337728d012584
MainasuK <cirno.mainasuk@gmail.com>
2015-01-09 19:36:55 +0800

    ex.09_Fibonacci no more than 10 000

--- ch4/ex09_Fibonacci_v2/ex09_Fibonacci_v2/main.c
+++ ch4/ex09_Fibonacci_v2/ex09_Fibonacci_v2/main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  ex08_Fibonacci
+//  ex09_Fibonacci_v2
 //
 //  Created by Cirno MainasuK on 2015-1-9.
 //  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
@@ -12,15 +12,19 @@ int fibonacci(int);
 
 //  This program list the Fibonacci sequence
 //  the function fibonacci get the result of F(index)
+//  and the result less than 10 000
 int main(int argc, const char * argv[]) {
     printf("This program lists the Fibonacci sequence.\n");
     
     printf(" F(%d) = %4d\n", 0, 0);
     printf(" F(%d) = %4d\n", 1, 1);
     
-    for (int i = 2; i <= 15; i++) {
+    for (int i = 2; ; i++) {
         if (i < 10) printf(" ");        //  aline prints
+        if (fibonacci(i) >= 10000) break;
+        
         printf("F(%d) = %4d\n", i, fibonacci(i));
+        
     }
     return 0;
 }
