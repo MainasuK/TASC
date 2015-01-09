d6abcab0df02d0a0496e972d0281dbfa8044956b
MainasuK <cirno.mainasuk@gmail.com>
2015-01-09 17:23:29 +0800

    ex.06_liftoff v2 : count from 10 to 0
     - do while

---
+++ ch4/ex06_liftoff_v2/ex06_liftoff_v2/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  ex06_liftoff_v2
+//
+//  Created by Cirno MainasuK on 2015-1-9.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+#include <stdio.h>
+#define StartingCount 10
+
+int main(int argc, const char * argv[]) {
+    int t = StartingCount;
+    do {
+        printf("%2d\n", t);
+    } while (t--);
+        
+    
+    printf("Liftoff!\n");
+    
+    return 0;
+}
