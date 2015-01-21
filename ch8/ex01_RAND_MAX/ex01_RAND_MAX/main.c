eb99e3da56e0fa8b13c459aef5ad94018ea34b74
MainasuK <cirno.mainasuk@gmail.com>
2015-01-21 22:14:02 +0800

    ex.01_RAND_MAX

---
+++ ch8/ex01_RAND_MAX/ex01_RAND_MAX/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  ex01_RAND_MAX
+//
+//  Created by Cirno MainasuK on 2015-1-21.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    
+    printf("On this computer, RAND_MAX = %d.\n", RAND_MAX);
+    
+    //  On this computer, RAND_MAX = 2147483647.
+    //  Mac OS X 10.10
+    return 0;
+}
