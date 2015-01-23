5cb468265d991c4ce407381f3d81ef437bc9fbb7
MainasuK <cirno.mainasuk@gmail.com>
2015-01-23 21:56:04 +0800

    ex.09_math quiz v2

--- ch8/ex08_math_quiz/ex08_math_quiz/main.c
+++ ch8/ex08_math_quiz/ex08_math_quiz/main.c
@@ -12,6 +12,7 @@
 #define TIMES 5
 
 void questions(int times);
+void sayCorrect(int sum);
 
 int main(int argc, const char * argv[]) {
     questions(TIMES);
@@ -21,7 +22,7 @@ int main(int argc, const char * argv[]) {
 
 void questions(int times) {
     randomize();
-    int x, y, sum, answer, correct;
+    int x, y, sum, answer, correct, loop;
     int op;
     int ref, c;
     while (times) {
@@ -30,26 +31,54 @@ void questions(int times) {
         op = randomChance(0.5);
         correct = 1;
         sum = (op) ? x+y : x-y;
+        loop = 0;
+        
         if (sum > 0 && sum < 20) {
             printf("What is %d %s %d? ", x, (op) ? "+" : "-", y);
             while (correct) {
                 ref = scanf("%d", &answer);
                 while ((c=getchar()) != '\n')
                     ;
+                if (loop == 2) {
+                    printf("No, the answer is %d.\n", sum);
+                    times--;
+                    break;
+                }
                 if (ref != 0) {
                     if (answer == sum) {
-                        printf("That's the answer!\n");
+                        sayCorrect(sum);
                         correct = 0;
                         times--;
                     } else {
                         printf("That's incorrect. Try a different answer: ");
+                        loop++;
                     }
                 } else {
                     printf("That's incorrect. Try a different answer: ");
-
+                    loop++;
                 }
             }
         }
-        
     }
+}
+
+void sayCorrect(int sum) {
+    int i;
+    i = randomInteger(1, 4);
+    switch (i) {
+        case 1:
+            printf("Correct!\n");
+            break;
+        case 2:
+            printf("You got it. The answer is %d.\n", sum);
+            break;
+        case 3:
+            printf("That's answer.\n");
+            break;
+        case 4:
+            printf("That's right.\n");
+        default:
+            break;
+    }
+    
 }
