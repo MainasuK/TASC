b511ecef2cc66681e840c33c43497c837e44f9c6
MainasuK <cirno.mainasuk@gmail.com>
2015-01-25 08:33:00 +0800

    ex.01_typedef enum

---
+++ ch9/ex01_enum/ex01_enum/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  ex01_enum
+//
+//  Created by Cirno MainasuK on 2015-1-25.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    typedef enum {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Violet
+    } colorT;
+    return 0;
+}
